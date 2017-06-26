6c8104f6370d8397023d198cace46bca4f03417e
Kevin Glover <ktg@cs.nott.ac.uk>
2017-06-26 11:04:56 +0100

    Move shower head movement all to one graph

--- js/query.js
+++ js/query.js
@@ -1,4 +1,9 @@
-function query(query) {
+function query(query, axis) {
 	return fetch("http://jzcpi1.local:8086/query?db=shower&epoch=ms&q=" + query)
 		.then((res) => res.json())
+		.then((json) => {
+		"use strict";
+			json.axis = axis;
+			return json;
+		})
 }
